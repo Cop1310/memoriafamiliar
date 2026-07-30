bac1a4ec301943bbbe5522f01e9f48b5f4f08026
Cop1310 <cop1310@hotmail.com>
2026-07-30 13:17:18 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Cambia VERSION en cada despliegue para forzar la actualización en Android/Chrome.
-const VERSION = "mf-v26";
+const VERSION = "mf-v27";
 const ARCHIVOS = ["./", "./index.html", "./admin.html", "./db.js", "./manifest.json", "./fondo.jpg"];
 
 self.addEventListener("install", e => {
